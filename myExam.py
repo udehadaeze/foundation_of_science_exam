ed6f4f3a7d3d8876edece211e4e932885e647a26
udehadaeze <148863379+udehadaeze@users.noreply.github.com>
2024-01-26 10:17:27 +0100

    Update myExam.py

--- myExam.py
+++ myExam.py
@@ -1,39 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'  # Database URI
 db = SQLAlchemy(app)
 
+# Book model
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     author = db.Column(db.String(255))
     publication_year = db.Column(db.Integer)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# Initialize database
+@app.before_first_request
+def addcontext():
+    with app.app_context():
+        db.create_all()  # Create tables if not exist
 
-@app.route("/books")
+# Route for displaying book list
+@app.route('/books')
 def books():
-    books = Book.query.all()
+    books = Book.query.all()  # Get all books from database
     return render_template('books.html', books=books)
 
-@app.route("/add_book", methods=['GET', 'POST'])
+# Route for adding a new book
+@app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
-        publication_year = int(request.form['publication_year'])
+        publication_year = request.form['publication_year']
 
         new_book = Book(title=title, author=author, publication_year=publication_year)
-        db.session.add(new_book)
-        db.session.commit()
-        return redirect(url_for('books'))
-    else:
-        return render_template('add_book.html')
+        db.session.add(new_book)  # Add new book to database
+        db.session.commit()  # Save changes
+
+        return redirect('/')  # Redirect to main page
+
+    return render_template('add_book.html')
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
